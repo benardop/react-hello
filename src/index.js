5cef4a2b75af52c28acd49b21afc7533b1e903d7
benardop <benard.oc.p@gmail.com>
2023-12-05 17:38:41 -0300

    Update index file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import ReactDOM  from 'react-dom/client'
 
 function HelloWorld() {
   return(
-    <div>Hello World!</div>
+    <div>Hello World</div>
   )
 }
 
+function Hello() {
+  return(
+    <span>World</span>
+  )
+}
+
+function World() {
+  return(
+    <span>World</span>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <HelloWorld/>
+    <Hello/> <World/>
   </React.StrictMode>
 )
